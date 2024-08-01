c09930b79911cd525ac13ebe6571ed2388fce920
monzondeveloper <monzondeveloper@gmail.com>
2024-08-01 14:06:07 -0300

    unit tests with assert and fluent assertions

---
+++ UnitTestFluentAssertionsExample/Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string FullName { get; set; }
+
+        public User(string fullName)
+        {
+            FullName = fullName;
+            SplitFullName();
+        }
+
+        private void SplitFullName()
+        {
+            var names = FullName.Split(' ');
+            FirstName = names[0];
+            LastName = names[1];
+        }
+    }
+}
---
+++ UnitTestFluentAssertionsExample/DomainTests/UserTests.cs
@@ -0,0 +1,59 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainTests
+{
+    public class UserTests
+    {
+        [Theory]
+        [InlineData("John Cast")]
+        [InlineData("Alex Mountain")]
+        public void TestFullName(string fullName)
+        {
+            // Arrange
+            var user = new User(fullName);
+
+            // Act
+            var actual = user.FullName;
+
+            // Assert
+            Assert.Equal(fullName, actual);
+        }
+
+        [Theory]
+        [InlineData("Cast", "John Cast")]
+        [InlineData("Mountain", "Alex Mountain")]
+        public void TestLastName(string lastName, string fullName)
+        {
+            // Arrange
+            var user = new User(fullName);
+
+            // Act
+            var actual = user.LastName;
+
+            // Assert
+            Assert.Equal(lastName, actual);
+        }
+
+        [Theory]
+        [InlineData("John", "Mark Two", "John Mark Two")]
+        public void TestingEdgeCaseNames(string firstName, string lastName, string fullName)
+        {
+            // Arrange
+            var user = new User(fullName);
+
+            using var _ = new AssertionScope();
+
+            user.FirstName.Should().Be(firstName);
+
+            user.LastName.Should()
+                .StartWith(lastName.Substring(0, 4)); // Mark
+        }
+    }
+}
